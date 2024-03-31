e4f4ee27380cdebfabf3036bd83c1c262ceb8bd4
Jordan <jordankleinbaum1@gmail.com>
2024-03-31 01:28:27 -0400

    Adjusted DataClass and SQL Text of Previous SPending Analysis

--- SignalRChat/Pages/DataClasses/PreviousSpendingAnalysis.cs
+++ SignalRChat/Pages/DataClasses/PreviousSpendingAnalysis.cs
@@ -5,7 +5,7 @@
         public int SpendingAnalysisID { get; set; }
         public string SpendingAnalysisName { get; set; }
         public string? SpendingAnalysisDescription { get; set; }
+        public string? BasedOffOf { get; set; }
         public DateTime? SpendingAnalysisDate { get; set; }
-
     }
 }
