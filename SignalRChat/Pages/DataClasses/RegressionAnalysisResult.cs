20068ed7ca10d913e5cb902c8a260afff2dfc7be
Jordan <jordankleinbaum1@gmail.com>
2024-04-02 01:18:36 -0400

    I ditched ChartJS. Wasn't doing it for me but HOLY COW THIS IS INSANE. PLOTLYJS LETS GOOOO SO CLUTCH THIS IS GORGEOUS

---
+++ SignalRChat/Pages/DataClasses/RegressionAnalysisResult.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRChat.Pages.DataClasses
+{
+    public class RegressionAnalysisResult
+    {
+        public double Intercept { get; set; }
+        public double Slope { get; set; }
+        public double RSquared { get; set; }
+    }
+}
--- SignalRChat/Pages/RevenueAnalysis.cshtml.cs
+++ SignalRChat/Pages/RevenueAnalysis.cshtml.cs
@@ -1,132 +1,78 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using OfficeOpenXml; // You might need to install EPPlus package for Excel handling
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.ComponentModel.DataAnnotations;
-using SignalRChat.Pages.DataClasses;
-using SignalRChat.Pages.DB;
+using OfficeOpenXml;
+using MathNet.Numerics;
 
 namespace SignalRChat.Pages
 {
+    public class RegressionAnalysisResult
+    {
+        public double Intercept { get; set; }
+        public double Slope { get; set; }
+        public double RSquared { get; set; }
+    }
+
     public class RevenueAnalysisModel : PageModel
     {
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly string _uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads");
 
-        public RevenueAnalysisModel(IWebHostEnvironment webHostEnvironment)
+        public RegressionAnalysisResult RegressionAnalysisResult { get; set; }
+        public List<double> XValues { get; set; }
+        public List<double> YValues { get; set; }
+        public List<double> Trendline { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string fileName)
         {
-            _webHostEnvironment = webHostEnvironment;
+            if (string.IsNullOrEmpty(fileName))
+                return RedirectToPage("/CollabHub");
+
+            var filePath = Path.Combine(_uploadsFolder, fileName);
+
+            if (!System.IO.File.Exists(filePath))
+                return RedirectToPage("/CollabHub");
+
+            await PerformAnalysisAsync(filePath);
+
+            return Page();
         }
 
-        // Chart Stuff
-        public List<string> XAxisData { get; private set; }
-        public List<int> YAxisData { get; private set; }
-        public string XAxisTitle { get; private set; }
-        public string YAxisTitle { get; private set; }
-
-        public List<string> Columns { get; set; }
-
-
-        // Insert into DataBase Stuff
-        [BindProperty]
-        [Required(ErrorMessage = "Analysis Title is required")]
-        public string AnalysisName { get; set; }
-
-        [BindProperty]
-        [Required(ErrorMessage = "Analysis Description is required")]
-        public string AnalysisDescription { get; set; }
-
-        public void OnGet(string fileName)
+        private async Task PerformAnalysisAsync(string filePath)
         {
-            string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads");
-            string filePath = Path.Combine(uploadsFolder, fileName);
+            using var package = new ExcelPackage(new FileInfo(filePath));
+            var worksheet = package.Workbook.Worksheets.FirstOrDefault();
 
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            if (worksheet == null)
+                return;
+
+            var rowCount = worksheet.Dimension.Rows;
+            var colCount = worksheet.Dimension.Columns;
+
+            var xData = new List<double>();
+            var yData = new List<double>();
+
+            for (int row = 2; row <= rowCount; row++)
             {
-                var worksheet = package.Workbook.Worksheets.FirstOrDefault();
-                if (worksheet != null)
-                {
+                var xValue = worksheet.Cells[row, 1].GetValue<double>();
+                var yValue = worksheet.Cells[row, 2].GetValue<double>();
 
-                    var columns = Enumerable.Range(1, worksheet.Dimension.End.Column)
-                                           .Select(col => worksheet.Cells[1, col].Value?.ToString())
-                                           .ToList();
-
-                    // Pass the columns to the Razor page
-                    Columns = columns;
-                    // Assuming the first row contains headers
-                    var xAxisTitle = worksheet.Cells["A1"].Value?.ToString();
-                    var yAxisTitle = worksheet.Cells["B1"].Value?.ToString();
-
-                    // Get data from columns A and B, starting from the second row
-                    var xAxisData = worksheet.Cells["A2:A" + worksheet.Dimension.End.Row]
-                                               .Select(cell => cell.Value?.ToString())
-                                               .ToList();
-
-                    var yAxisData = worksheet.Cells["B2:B" + worksheet.Dimension.End.Row]
-                                               .Select(cell => int.Parse(cell.Value?.ToString() ?? "0")) // Assuming integers in column B
-                                               .ToList();
-
-                    // Pass the data to the Razor page
-                    XAxisTitle = xAxisTitle;
-                    YAxisTitle = yAxisTitle;
-                    XAxisData = xAxisData;
-                    YAxisData = yAxisData;
-                }
-            }
-        }
-
-        public IActionResult OnPost(string fileName)
-        {
-            if (!ModelState.IsValid)
-            {
-                // If model state is not valid, return to the page
-                return Page();
+                xData.Add(xValue);
+                yData.Add(yValue);
             }
 
-            // Create a new PreviousSpendingAnalysis object
-            var spendingAnalysis = new PreviousSpendingAnalysis
+            XValues = xData;
+            YValues = yData;
+
+            // Perform linear regression
+            var regression = Fit.Line(XValues.ToArray(), YValues.ToArray());
+            RegressionAnalysisResult = new RegressionAnalysisResult
             {
-                SpendingAnalysisName = AnalysisName,
-                SpendingAnalysisDescription = AnalysisDescription,
-                BasedOffOf = fileName
+                Intercept = regression.Item1,
+                Slope = regression.Item2,
+                RSquared = GoodnessOfFit.RSquared(YValues, XValues.Select(x => regression.Item1 + regression.Item2 * x))
             };
 
-            // Insert the collaboration into the database
-            DBClass.InsertPreviousSpendingAnalysis(spendingAnalysis);
-
-            // Redirect to the CollabHub page or any other page as needed
-            return RedirectToPage("CollabHub");
+            // Generate trendline
+            Trendline = XValues.Select(x => RegressionAnalysisResult.Intercept + RegressionAnalysisResult.Slope * x).ToList();
         }
-
-        public IActionResult OnGetGetData(string xAxisColumn, string yAxisColumn)
-        {
-            string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads");
-            string filePath = Path.Combine(uploadsFolder, Request.Query["fileName"]);
-
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
-            {
-                var worksheet = package.Workbook.Worksheets.FirstOrDefault();
-                if (worksheet != null)
-                {
-                    var xAxisData = worksheet.Cells[$"{xAxisColumn}2:{xAxisColumn}{worksheet.Dimension.End.Row}"]
-                                               .Select(cell => cell.Value?.ToString())
-                                               .ToList();
-
-                    var yAxisData = worksheet.Cells[$"{yAxisColumn}2:{yAxisColumn}{worksheet.Dimension.End.Row}"]
-                                               .Select(cell => int.Parse(cell.Value?.ToString() ?? "0"))
-                                               .ToList();
-
-                    return new JsonResult(new { xAxisData = xAxisData, yAxisData = yAxisData });
-                }
-            }
-
-            return new JsonResult(null);
-        }
-
-
-
     }
 }
