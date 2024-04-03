c20b7e5c6d6b6d354c7aa63ba7abe2df0e6a3b4b
charweyher <charweyher@gmail.com>
2024-04-03 14:27:49 -0400

    Made the Public Document (document repository) the index page, allowing for a log in button that sends you to the Login page to log into the system. Added new layout for public doc page

--- SignalRChat/Pages/Index.cshtml.cs
+++ SignalRChat/Pages/Index.cshtml.cs
@@ -1,67 +1,80 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SignalRChat.Pages.DataClasses;
 using SignalRChat.Pages.DB;
+using System.Data.SqlClient;
 
 namespace SignalRChat.Pages
 {
     public class IndexModel : PageModel
     {
-        // I DONT KNOW WHAT THIS DOES BUT IT WAS HERE BY DEFAULT ON INDEX PAGE
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         [BindProperty]
-        public string Username { get; set; }
-        [BindProperty]
-        public string Password { get; set; }
-        [BindProperty]
-        public string Admin { get; set; }
+        public KnowledgeItem NewKnowledgeItem { get; set; }
+        public List<KnowledgeItem> KnowledgeItemList { get; set; } = new List<KnowledgeItem>();
 
-        public IActionResult OnGet(String logout)
+        public List<Users> UsersList { get; set; } = new List<Users>();
+
+        public string SearchTerm { get; set; }
+        public List<Document> Doc { get; set; } = new List<Document>();
+
+        public IActionResult OnGet()
         {
-            if (logout == "true")
-            {
-                HttpContext.Session.Clear();
-                ViewData["LoginMessage"] = "Successfully Logged Out!";
-            }
-
-            if (HttpContext.Session.GetString("username") != null)
-            {
-                return RedirectToPage("CollabHub");
-            }
-
-            return Page();
-        }
-
-        public IActionResult OnPost()
-        {
-
-            if (DBClass.sp_Lab3Login(Username, Password))
-            {
-                HttpContext.Session.SetString("username", Username);
+                // Populate UsersList with user data from the database
+                SqlDataReader userReader = DBClass.GetAllUsers();
+                while (userReader.Read())
+                {
+                    UsersList.Add(new Users
+                    {
+                        UserID = Convert.ToInt32(userReader["UserID"]),
+                        Username = userReader["Username"].ToString()
+                    });
+                }
+                userReader.Close();
                 DBClass.CollabFusionDBConnection.Close();
-                DBClass.AuthDBConnection.Close();
-                return RedirectToPage("CollabHub");
-            }
 
+                LoadAllKnowledge();
+                // Close your connection in DBClass
+                DBClass.CollabFusionDBConnection.Close();
+                return Page();
+
+        }
+
+
+        public IActionResult OnPostSearch(String SearchTerm)
+        {
+            if (!string.IsNullOrEmpty(SearchTerm))
+            {
+                Doc = DB.DBClass.SearchKnowledge(SearchTerm);
+
+            }
             else
             {
-                ViewData["LoginMessage"] = "Username and/or Password Incorrect";
-                DBClass.CollabFusionDBConnection.Close();
-                DBClass.AuthDBConnection.Close();
+                LoadAllKnowledge();
+                //DBClass.CollabFusionDBConnection.Close();
 
-                return Page();
             }
-        }
-
-        public IActionResult OnPostLogoutHandler()
-        {
-            HttpContext.Session.Clear();
             return Page();
+        }
+        private void LoadAllKnowledge()
+        {
+            //Document LOGIC START
+            //Document LOGIC START
+            SqlDataReader reader = DBClass.GeneralReaderQuery("SELECT * FROM Document Order By DateAdded Desc");
+
+            while (reader.Read())
+            {
+                Doc.Add(new Document
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    FileName = reader["FileName"].ToString(),
+                    FileData = (byte[])reader["FileData"],
+                    DateAdded = Convert.ToDateTime(reader["DateAdded"]),
+                    AnalysisType = reader["AnalysisType"].ToString()
+                });
+            }
+            reader.Close();
+            DB.DBClass.CollabFusionDBConnection.Close();
+
         }
     }
 }
