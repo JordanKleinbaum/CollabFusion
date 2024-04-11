026e57c1d1b0aa2d8df7e0203ce63bc21d13dd8e
Jordan <jordankleinbaum1@gmail.com>
2024-04-11 16:00:24 -0400

    Adjusted the txt file and the Class

---
+++ SignalRChat/Pages/DataClasses/PublicPreviousSpendingAnalysis.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalRChat.Pages.DataClasses
+{
+    public class PublicPreviousSpendingAnalysis
+    {
+        public int PublicSpendingAnalysisID { get; set; }
+        public int SpendingAnalysisID { get; set; }
+        public string SpendingAnalysisName { get; set; }
+        public string? SpendingAnalysisDescription { get; set; }
+        public string? BasedOffOf { get; set; }
+        public DateTime SpendingAnalysisDate { get; set; }
+        public int Column1 { get; set; }
+        public int Column2 { get; set; }
+    }
+}
